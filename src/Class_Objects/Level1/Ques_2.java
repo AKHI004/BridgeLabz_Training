96df66cc67067009e54c03713fe93fac54f95458
Akshat <akhi142004@gmail.com>
2025-09-23 00:12:40 +0530

    Assignment 9

---
+++ src/Class_Objects/Level1/Ques_2.java
@@ -0,0 +1,34 @@
+package Class_Objects.Level1;
+
+public class Ques_2 {
+    public static class Circle {
+        double radius;
+
+        Circle(double radius) {
+            this.radius = radius;
+        }
+
+        double Area() {
+            return Math.PI * radius * radius;
+        }
+
+        double Circumference() {
+            return 2 * Math.PI * radius;
+        }
+
+        void show() {
+            System.out.println("Radius        : " + radius);
+            System.out.println("Area          : " + Area());
+            System.out.println("Circumference : " + Circumference());
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Circle c1 = new Circle(5.0);
+        Circle c2 = new Circle(7.5);
+
+        c1.show();
+        c2.show();
+    }
+}
---
+++ src/Class_Objects/Level1/Ques_3.java
@@ -0,0 +1,30 @@
+package Class_Objects.Level1;
+
+public class Ques_3 {
+    public static class Book {
+        String title;
+        String author;
+        double price;
+
+        Book(String title, String author, double price) {
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+
+        void show() {
+            System.out.println("Book Title : " + title);
+            System.out.println("Author     : " + author);
+            System.out.println("Price      : ₹" + price);
+            System.out.println();
+        }
+    }
+
+    public static void main(String... args) {
+        Book b1 = new Book("Gitanjali", "Rabindranath Tagore", 200);
+        Book b2 = new Book("The White Tiger", "Aravind Adiga", 350);
+
+        b1.show();
+        b2.show();
+    }
+}
